c7b92f823c7579fbb4ec08cc6e3125ba2cb53736
Joseph <joseph.duf@gmail.com>
2022-01-20 16:59:49 +0100

    update

--- include/dataCat2.php
+++ include/dataCat2.php
@@ -5,6 +5,5 @@ $histoire =[
     'presentation' => 'Dans ce site vous découvrirez l\'histiore du mouvement de LGBT ainsi que leurs luttes.',
     'pave' => 'Les plus ancienne histiores de personnes LGBT nous proviennent de l\'Antiquité.',
     'titretab' => 'Tableau des philosophe & textes LGBT antique',
-
-
+    
 ];
